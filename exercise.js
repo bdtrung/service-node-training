1e4d1d4a543157824be961f9458faff894e98bee
prime bui <trung.bui@overdose.digital>
2023-06-20 17:32:42 +0700

    template

--- exercise.js
+++ exercise.js
@@ -84,3 +84,24 @@ const getPostDataId = async (id) => {
 
 //requirement 8
 // console.dir(await getPostDataId(), {depth: null})
+
+
+const person = {
+    id: 1,
+    name: "John Doe",
+};
+
+const job = {job: 'NodeJs Developer'};
+const mergePerson = {...person, ...job};
+console.log(mergePerson);
+
+// You can use this to pick/omit fields from object
+const post = {
+    "userId": 1,
+    "id": 1,
+    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+};
+
+const {userId, id, ...postSelected} = post; // post object should contain only title and body
+console.log(postSelected);
--- koa-exercise/src/app.js
+++ koa-exercise/src/app.js
@@ -1,8 +1,18 @@
 const Koa = require('koa');
 const koaBody = require('koa-body');
 const routes = require('./routes/routes.js');
+const render = require('koa-ejs');
+const path = require('path');
 
 const app = new Koa();
+
+render(app, {
+    root: path.join(__dirname, 'view'),
+    layout: 'template',
+    viewExt: 'html',
+    cache: false,
+    debug: true
+});
 
 app.use(koaBody());
 app.use(routes.routes());
