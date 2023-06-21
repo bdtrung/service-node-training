ce2377c6d839197f8e4c08101782a1f84a29f052
Prime Bui <trungbd@mageplaza.com>
2023-06-21 23:20:58 +0700

    add mongooes

--- koa-exercise/src/app.js
+++ koa-exercise/src/app.js
@@ -3,8 +3,12 @@ const koaBody = require('koa-body');
 const routes = require('./routes/routes.js');
 const render = require('koa-ejs');
 const path = require('path');
+const mongoose = require('mongoose');
+
 
 const app = new Koa();
+
+mongoose.connect('mongodb://localhost:27017/koadb');
 
 render(app, {
     root: path.join(__dirname, 'view'),
